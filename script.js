a1b84c42a704e653c80a48accc4fe83e2c18ec16
cemÄilcakmak <b.cemilcakmak@gmail.com>
2023-04-06 23:02:57 +0300

    Implemented darkening when mouseover

--- script.js
+++ script.js
@@ -5,8 +5,24 @@ const squareContainer = document.querySelector('#square-container');
 mainContainer.appendChild(squareContainer);
 
 for (let i = 0; i < 256; i++) {
-    const square = document.createElement('div');
-    square.classList.add('cell');
-
-    squareContainer.appendChild(square);
+    let cell = document.createElement('div');
+    cell.classList.add('cell');
+    cell.setAttribute('key', i);
+    squareContainer.appendChild(cell);
 }
+
+squareContainer.addEventListener('mouseover', function (e) {
+    handleEvent(e);
+});
+
+function handleEvent(e) {
+    let cell = document.querySelector(`[key='${e.target.getAttribute('key')}']`);
+    console.log(cell);
+    let computedColor = window.getComputedStyle(cell).getPropertyValue('background-color');
+    cell.style.backgroundColor = calculate10percentageDarker(computedColor);
+}
+
+function calculate10percentageDarker(computedColor) {
+    let colorArray = computedColor.match(/\d+/g);
+    return `rgb(${colorArray[0] - 26}, ${colorArray[1] - 26}, ${colorArray[2] - 26})`;
+  }
