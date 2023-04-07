195436b9dd1c626ea17ddb6f52182adfd5511c5c
cemÄilcakmak <b.cemilcakmak@gmail.com>
2023-04-07 18:25:47 +0300

    Implemented a dynamic grid size

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
 const body = document.querySelector('body');
 const mainContainer = document.querySelector('#main-container')
 const squareContainer = document.querySelector('#square-container');
+const button = document.querySelector('#grid-size');
 
 mainContainer.appendChild(squareContainer);
+const size = parseInt(squareContainer.getAttribute('size'));
 
-for (let i = 0; i < 256; i++) {
-    let cell = document.createElement('div');
-    cell.classList.add('cell');
-    cell.setAttribute('key', i);
-    squareContainer.appendChild(cell);
-}
+createCells(16);
 
 let isMouseDown = false;
 squareContainer.addEventListener('mousedown', function () {
@@ -24,6 +21,15 @@ squareContainer.addEventListener('mouseover', function (e) {
   }
 });
 
+button.addEventListener('click', () => {
+  const size = prompt('Enter a number from 1 to 100');
+  const cells = document.querySelectorAll('.cell');
+  cells.forEach(cell => {
+    cell.remove();
+  });
+  createCells(size);
+});
+
 function handleEvent(e) {
     let cell = document.querySelector(`[key='${e.target.getAttribute('key')}']`);
     console.log(cell);
@@ -34,4 +40,15 @@ function handleEvent(e) {
 function calculate10percentageDarker(computedColor) {
     let colorArray = computedColor.match(/\d+/g);
     return `rgb(${colorArray[0] - 26}, ${colorArray[1] - 26}, ${colorArray[2] - 26})`;
-  }
+}
+  
+function createCells(size) {
+  for (let i = 0; i < size*size; i++) {
+    let cell = document.createElement('div');
+    cell.classList.add('cell');
+    cell.style.width = `calc(100% / ${size})`;
+    cell.style.height = `calc(100% / ${size})`;
+    cell.setAttribute('key', i);
+    squareContainer.appendChild(cell);
+  }  
+}
